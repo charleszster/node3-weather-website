4da7088eb249c57013c8192e5908620b8103851b
Charles Zivancev <charles@cszgroup.com>
2020-06-17 13:11:21 -0400

    Added humidity to forecast.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.  Try another search', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ".  The temperature is " + body.current.temperature + " degrees out.  There is a " + body.current.precip + "% chance of rain.")
+            callback(undefined, body.current.weather_descriptions[0] + ".  The temperature is " + body.current.temperature + " degrees out.  There is a " + body.current.precip + "% chance of rain." + "The humidity is " + body.current.humidity + "%.")
         }
     })
 }
